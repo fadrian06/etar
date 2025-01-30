aaedaac8099a446da199f1b8488bee0488e37809
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-29 21:55:57 -0400

    add spanish translations

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,8 +2,8 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
+        <div class="font-medium text-red-600" lang="es">
+            {{ __('¡Ups! Algo salió mal.') }}
         </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
